cb00a09d4947e06fe86204f94f54776bb0a85bbe
hieuduong61 <hieuduong612001@gmail.com>
2022-11-18 23:40:46 -0800

    First Lines of Code

---
+++ Actor.h
@@ -0,0 +1,47 @@
+#ifndef ACTOR_H_
+#define ACTOR_H_
+
+#include "GraphObject.h"
+// Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+
+//Actor Declare
+class Actor: public GraphObject
+{
+public:
+    Actor(int imageID, int startX, int startY, int hitPoints);
+    
+    virtual ~Actor();
+    
+    virtual int getHitpoints() const;
+    
+    virtual void setHitpoints(int value);
+    
+    virtual void doSomething() = 0;
+    
+private:
+    int m_hitPoints;
+};
+
+
+
+//Tunnelman Declare
+const int DEFAULT_HITPOINTS = 10;
+const int DEFAULT_WATER = 5;
+const int DEFAULT_SONARCHARGE = 1;
+const int DEFAULT_GOLDNUGGET = 0;
+
+class Tunnelman: public Actor
+{
+public:
+    Tunnelman(int imageID, int startX, int startY, int hitPoints = DEFAULT_HITPOINTS, int water = DEFAULT_WATER, int sonarCharge = DEFAULT_SONARCHARGE, int goldNugget = DEFAULT_GOLDNUGGET);
+    
+    virtual ~Tunnelman();
+    
+    virtual void doSomething();
+    
+private:
+    int m_water; //start with 5
+    int m_sonarCharge; //start with 1
+    int m_goldNugget; // start with 0
+};
+#endif // ACTOR_H_
