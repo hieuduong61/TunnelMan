2f02d953974d1d33b355e101f3783aa58f107182
hieuduong61 <hieuduong612001@gmail.com>
2022-11-27 23:09:34 -0800

    TunnelMan mostly works but haven't finish the dig function

--- Actor.cpp
+++ Actor.cpp
@@ -1,50 +1,120 @@
 #include "Actor.h"
-#include "StudentWorld.h"
+#include "GameWorld.h"
 #include "GameConstants.h"
+#include "StudentWorld.h"
 // Students:  Add code to this file (if you wish), Actor.h, StudentWorld.h, and StudentWorld.cpp
 
 
 
 //BEGIN ACTOR DEF
 ///
-Actor::Actor(int imageID, int startX, int startY, int hitPoints):m_hitPoints(hitPoints), GraphObject(imageID, startX, startY){}
+Actor::Actor(StudentWorld* world,int imageID, int startX, int startY, Direction dir, double size, unsigned int depth, int hp):m_world(world), m_HP(hp), GraphObject(imageID, startX, startY, dir, size, depth){}
 
 Actor::~Actor(){}
 
-int Actor::getHitpoints() const {return m_hitPoints;}
+StudentWorld* Actor::getWorld(){return m_world;}
 
-void Actor::setHitpoints(int value) {m_hitPoints = value;}
+int Actor::getHP() const {return m_HP;}
+
+void Actor::setHP(int value) {m_HP = value;}
+
+bool Actor::m_isDead() {return ((m_HP <= 0) ? true:false);}
+
 //END ACTOR DEF
 
 
 //BEGIN TUNNELMAN DEF
-Tunnelman::Tunnelman(int startX, int startY, int imageID,  int hitPoints, int water, int sonarCharge, int goldNugget):Actor(imageID, startX, startY, hitPoints), m_water(water), m_sonarCharge(sonarCharge), m_goldNugget(goldNugget)
+Tunnelman::Tunnelman(StudentWorld* world,int startX, int startY, int imageID,  int hp, int water, int sonarCharge, int goldNugget):Actor(world, imageID, startX, startY, right, 1.0, 0, hp), m_water(water), m_sonarCharge(sonarCharge), m_goldNugget(goldNugget)
 {
     setVisible(true);
 }
 
+void Tunnelman::moveToDir(Direction d)
+{
+    const int MAX_UP = 63;
+    const int MAX_DOWN = 3;
+    const int MAX_LELF = 0;
+    const int MAX_RIGHT = 59;
+    switch(d)
+    {
+        case none:
+            break;
+        case left:
+            if (getDirection() != left)
+                setDirection(d);
+            else
+            {
+                if (getX() != MAX_LELF)
+                    moveTo(getX() - 1, getY());
+            }
+            break;
+        case right:
+            if (getDirection() != right)
+                setDirection(d);
+            else
+            {
+                if (getX() != MAX_RIGHT)
+                    moveTo(getX() + 1, getY());
+            }
+            break;
+        case up:
+            if (getDirection() != up)
+                setDirection(d);
+            else
+            {
+                if (getY() != MAX_UP)
+                    moveTo(getX(), getY() + 1);
+            }
+            break;
+        case down:
+            if (getDirection() != down)
+                setDirection(d);
+            else
+            {
+                if (getY() != MAX_DOWN)
+                    moveTo(getX(), getY() - 1);
+            }
+            break;
+    }
+}
+
+bool Tunnelman::dig()
+{
+    return true;
+}
+
 void Tunnelman::doSomething()
 {
-    if (getHitpoints() == 0)
+    if (m_isDead())
         return;
     
-    /*int ch;
-    //change something
-    if (getWorld()->getKey(ch) == true)// user hit a key this tick!
+    if (dig())
     {
-        switch (ch)
+        getWorld()->playSound(SOUND_DIG);
+    }
+    
+    int key;
+    if (getWorld()->getKey(key) == true)// user hit a key this tick!
+    {
+        switch (key)
         {
-            case KEY_PRESS_LEFT:
-                //implement
+            case none:
                 break;
-            case KEY_PRESS_RIGHT:
+            case left:
                 //implement
+                moveToDir(left);
                 break;
-            case KEY_PRESS_UP:
+            case right:
                 //implement
+                moveToDir(right);
                 break;
-            case KEY_PRESS_DOWN:
+            case up:
                 //implement
+                moveToDir(up);
+                break;
+            case down:
+                //implement
+                moveToDir(up);
                 break;
             case KEY_PRESS_SPACE:
                 //implement
@@ -56,7 +126,7 @@ void Tunnelman::doSomething()
                 //implement
                 break;
         }
-    }*/
+    }
 }
 
 Tunnelman::~Tunnelman(){}
@@ -64,11 +134,16 @@ Tunnelman::~Tunnelman(){}
 
 
 //BEGIN DEF EARTH
-Earth::Earth(int startX, int startY,int imageID):GraphObject(imageID, startX, startY, left, 0.5, 3)
+Earth::Earth(StudentWorld* world, int startX, int startY, int imageID, Direction dir, double size, unsigned int depth, int hp):Actor(world,imageID, startX, startY, dir, size, depth, hp)
 {
     setVisible(true);
 };
+
 Earth::~Earth()
-{};
+{
+    setVisible(false);
+};
+
+void Earth::doSomething(){}
 
 //END DEF EARTH
--- Actor.h
+++ Actor.h
@@ -2,24 +2,26 @@
 #define ACTOR_H_
 
 #include "GraphObject.h"
+#include "StudentWorld.h"
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
 
 //Actor Declare
 class Actor: public GraphObject
 {
 public:
-    Actor(int imageID, int startX, int startY, int hitPoints);
+    Actor(StudentWorld* world, int imageID, int startX, int startY, Direction dir, double size, unsigned int depth, int hitPoints);
     
     virtual ~Actor();
     
-    virtual int getHitpoints() const;
-    
-    virtual void setHitpoints(int value);
-    
+    virtual StudentWorld* getWorld();
+    virtual int getHP() const;
+    virtual void setHP(int value);
     virtual void doSomething() = 0;
-    
+    virtual bool m_isDead();
+
 private:
-    int m_hitPoints;
+    StudentWorld* m_world;
+    int m_HP;
 };
 
 
@@ -33,11 +35,12 @@ const int DEFAULT_GOLDNUGGET = 0;
 class Tunnelman: public Actor
 {
 public:
-    Tunnelman(int startX, int startY, int imageID = TID_PLAYER, int hitPoints = DEFAULT_HITPOINTS, int water = DEFAULT_WATER, int sonarCharge = DEFAULT_SONARCHARGE, int goldNugget = DEFAULT_GOLDNUGGET);
+    Tunnelman(StudentWorld* world, int startX, int startY, int imageID = TID_PLAYER, int hitPoints = DEFAULT_HITPOINTS, int water = DEFAULT_WATER, int sonarCharge = DEFAULT_SONARCHARGE, int goldNugget = DEFAULT_GOLDNUGGET);
     
     virtual ~Tunnelman();
-    
     virtual void doSomething();
+    virtual bool dig();
+    virtual void moveToDir(Direction d);
     
 private:
     int m_water; //start with 5
@@ -46,19 +49,55 @@ private:
 };
 //
 
+
+//ENEMY DECLARE
+class Enemy: public Actor
+{
+public:
+    Enemy(StudentWorld* world, int startX, int startY, int imageID, Direction dir, double size, unsigned int depth, int hitPoints);
+    virtual void doSomething() override;
+    virtual ~Enemy();
+};
+//
+
+//BOULDE DECLARE
+class Boulder: public Enemy
+{
+public:
+    Boulder(StudentWorld* world, int startX, int startY, int imageID = TID_BOULDER, Direction dir = down, double size = 1.0, unsigned int depth = 1, int hitPoints = 1);
+    ~Boulder();
+    virtual void doSomething() override;
+    bool ifState_stable();
+    bool ifState_waiting();
+    bool ifState_falling();
+    void setState_stable();
+    void setState_wating();
+    void setState_falling();
+private:
+    double radius;
+    double damage;
+    bool watingState;
+    bool stableState;
+    bool fallingState;
+};
+
 //COLLECTIBLE DECLARE
 class Collectible: public GraphObject
 {
-    Collectible(int imageID, int startX, int startY);
-    
+public:
+    Collectible(StudentWorld* world, int imageID, int startX, int startY);
     virtual ~Collectible();
 };
 //
 
+
 //EARTH DECLARE
-class Earth: public GraphObject
+class Earth: public Actor
 {
-    Earth(int startX, int startY,int imageID = TID_EARTH);
+public:
+    Earth(StudentWorld* world, int startX, int startY, int imageID = TID_EARTH, Direction dir = right, double size = 0.25, unsigned int depth = 3, int hitPoints = 1);
     virtual ~Earth();
+    
+    virtual void doSomething();
 };
 #endif // ACTOR_H_
